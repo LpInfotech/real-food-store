66ea5fbef7dbdac68aee097732acedc449d50a75
Mukesh <110394578+amanthakur2022@users.noreply.github.com>
2024-04-01 19:04:54 +0530

    changes

--- src/app/products/product/[id]/page.tsx
+++ src/app/products/product/[id]/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import data from '../../../api/data.json';
+import React, { useEffect,useRef } from 'react';
 
 
 const Page = ({ params }: { params: { id: string } }) => {
-	const id = params.id;
+	const id  = Number(params.id) ;
+	useEffect(() => {
+		const json = data.filter((product) => product.id === id);
+		console.log(json);
+	},[id])
+
 	return (
 		<div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
 			hello world {id}
