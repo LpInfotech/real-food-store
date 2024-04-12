20a74e49fa0f0d5f2c1a4a339f97433ab2e9e78b
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-04-12 18:43:44 +0530

    changed logo for both themes

--- src/app/ui/Navbar.jsx
+++ src/app/ui/Navbar.jsx
@@ -40,16 +40,12 @@ const Navbar = ({ sendDataToParent = null, isSearch = false }) => {
 	return (
 		<header>
 			<nav className="relative py-4 md:px-20 px-5 flex justify-between items-center shadow shadow-slate-300 bg-white dark:bg-slate-800">
-				{!searchIsOpen && (
-					<Link href="/" className="w-full md:w-auto md:hidden">
-						<img src="https://static.wixstatic.com/media/358326_05d4ec90dbaa41189432cef1ef73f5e1~mv2.jpg/v1/fit/w_2500,h_1330,al_c/358326_05d4ec90dbaa41189432cef1ef73f5e1~mv2.jpg" alt="real-food-logo" width={90} />
-					</Link>
-				)}
+				{!searchIsOpen}
 				<Link href="/" className="dark:hidden md:flex items-end">
-					<img src="https://static.wixstatic.com/media/358326_05d4ec90dbaa41189432cef1ef73f5e1~mv2.jpg/v1/fit/w_2500,h_1330,al_c/358326_05d4ec90dbaa41189432cef1ef73f5e1~mv2.jpg" alt="real-food-logo" width={120} />
+					<img src="https://brainfoodstudio.com/wp-content/uploads/2018/07/brain-food-studio-logo-1200px.png" alt="brain-food-logo" className='md:w-36 w-24' />
 				</Link>
 				<Link href="/" className="hidden dark:flex items-end">
-					<img src="https://assets-global.website-files.com/5ed793f46ddd9dea20efd90b/5ed8c5796d65e00acb992a63_LOGO_FOOD.STUDIO.png" alt="real-food-logo" width={120} />
+					<img src="https://brainfoodstudio.com/wp-content/uploads/2018/07/brain-food-studio-logo-reverse-1200px.png" alt="brain-food-logo" className='md:w-36 w-24' />
 				</Link>
 				{/* added search bar */}
 				{isSearch && <div className='border w-1/3 hidden md:block relative rounded-lg'>
@@ -66,7 +62,7 @@ const Navbar = ({ sendDataToParent = null, isSearch = false }) => {
 				{/* added search bar */}
 				<div className="flex justify-end md:justify-center items-center w-full md:w-auto gap-x-6">
 					<div className='md:hidden'>
-						{isSearch && <FontAwesomeIcon onClick={search} icon={searchIsOpen ? faXmark : faSearch} width="20" />}
+						{isSearch && <FontAwesomeIcon onClick={search} className='dark:text-white' icon={searchIsOpen ? faXmark : faSearch} width="20" />}
 						{(searchIsOpen && isSearch) && (
 							<div className="max-w-md mx-auto w-full md:hidden">
 								<div className="relative w-full rounded-lg  bg-white overflow-hidden">
@@ -95,7 +91,7 @@ const Navbar = ({ sendDataToParent = null, isSearch = false }) => {
 							<FontAwesomeIcon icon={faUser} size='lg' />
 							<span className='ps-1'>User</span>
 						</Link>
-						<div className={`dropdown-content absolute top-16 -right-2 z-10 origin-top-right divide-y *:py-3 *:px-5 *:text-sm *:block *:text-gray-700 w-48 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${showDropdown ? 'block' : 'hidden'}`}>
+						<div className={`dropdown-content absolute top-20 -right-2 z-10 origin-top-right divide-y *:py-3 *:px-5 *:text-sm *:block *:text-gray-700 w-48 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${showDropdown ? 'block' : 'hidden'}`}>
 							<a href="#" className='hover:font-bold'>
 								<FontAwesomeIcon icon={faCircleUser} /><span className='ms-3'>View Profile</span>
 							</a>
@@ -115,7 +111,7 @@ const Navbar = ({ sendDataToParent = null, isSearch = false }) => {
 						<FontAwesomeIcon icon={faCircleHalfStroke} />
 							<span className='ps-1'>Theme</span>
 						</Link>
-						<div className={`dropdown-content absolute top-16 -right-2 z-10 origin-top-right divide-y *:py-3 *:px-5 *:text-sm *:block *:text-gray-700 w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${showThemeDropdown ? 'block' : 'hidden'}`}>
+						<div className={`dropdown-content absolute top-20 -right-2 z-10 origin-top-right divide-y *:py-3 *:px-5 *:text-sm *:block *:text-gray-700 w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${showThemeDropdown ? 'block' : 'hidden'}`}>
 							<button onClick={lightTheme} type="button" className='w-full hover:font-bold'>
 								<FontAwesomeIcon icon={faSun} /><span className='ms-3'>Light</span>
 							</button>
@@ -129,7 +125,7 @@ const Navbar = ({ sendDataToParent = null, isSearch = false }) => {
 			</nav>
 			{
 				(searchIsOpen && isSearch) && (
-					<div className="relative w-full hidden md:block  bg-white shadow-xl" id="search-content">
+					<div className="relative w-full hidden md:block bg-white shadow-xl" id="search-content">
 						<div className="container mx-auto py-4 text-black">
 							<input
 								className="w-full h-8 outline-none text-sm bg-white text-gray-700 px-2"
