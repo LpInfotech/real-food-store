ad1393ba09641556b53a3e31fab58c0ec3d700b5
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-04-30 19:09:02 +0530

    homepage and footer designing

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -15,16 +15,16 @@ const Home = (data) => {
         );
     }
     return (
-        <div className='bg-white dark:bg-slate-800 dark:text-white text-black'>
+        <div className='bg-white dark:bg-gray-950  text-black scroll-smooth'>
             <Navbar isSearch={true} />
             {/* hero section */}
-            <div id="first-slide" className="relative grid grid-cols-2 items-center bg-center h-screen bg-cover bg-[url('https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/1.jpg?v=1623471846')]">
+            <div id="first-slide" className="relative grid grid-cols-2 items-center bg-center h-screen bg-cover bg-[url('https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/1.jpg?v=1623471846')] dark:bg-[url(https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/2.jpg?v=1623471983)]">
                 <div className="col-span-1 ps-20">
                     <p className="text-xl font-medium tracking-wider mb-6">Quality always comes first</p>
                     <h1 className="text-8xl font-medium uppercase tracking-wide">Fresh Fruit</h1>
                     <h2 className="text-xl font-medium py-3 mb-12">Fruit is a good choice every day for us</h2>
                     <div className="flex justify-start gap-x-5">
-                        <button type="button" className="bg-black text-white py-4 px-10 font-medium hover:bg-red-600">Read More</button>
+                        <a href="#service" className="transition duration-300 ease-in-out"><button type="button" className="bg-black text-white py-4 px-10 font-medium hover:bg-red-600">Read More</button></a>
                         <a href="/" target="_blank"><button type="button" className="bg-white py-4 px-10 font-medium hover:bg-red-600 hover:text-white">Shop Now</button></a>
                     </div>
                 </div>
@@ -36,38 +36,38 @@ const Home = (data) => {
                 <input type="radio" id="slide1" name="slide" className="me-3 slider-button" defaultChecked />
                 <input type="radio" id="slide2" name="slide" className="slider-button" />
             </div> */}
-            
+
             {/* why choose section */}
-            <div className="text-center py-8 gap-x-20 px-10">
-                <h2 className="text-5xl mt-5 mb-16">Why Choose Us?</h2>
-                <div className="grid grid-cols-4">
+            <div className="text-center py-8 gap-x-20 px-10" id="service">
+                <h2 className="text-5xl mt-5 mb-16 dark:text-white">Why Choose Us?</h2>
+                <div className="grid grid-cols-4 dark:text-white">
                     <div className="col-span-1 grid justify-items-center">
                         <img src="https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/1.png?v=1623465035" alt="support" />
                         <h3 className="uppercase font-semibold my-2">24/7 Friendly Support</h3>
-                        <p className="text-base font-medium text-gray-600">Our support team always ready for you to 7 days a week</p>
+                        <p className="text-base font-medium text-gray-600 dark:text-white">Our support team always ready for you to 7 days a week</p>
                     </div>
                     <div className="col-span-1 grid justify-items-center">
                         <img src="https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/2.png?v=1623465048" alt="free shipping" />
                         <h3 className="uppercase font-semibold my-2">Free Shipping</h3>
-                        <p className="text-base font-medium text-gray-600">Free worldwide shipping on all area orders above $100</p>
+                        <p className="text-base font-medium text-gray-600 dark:text-white">Free worldwide shipping on all area orders above $100</p>
                     </div>
                     <div className="col-span-1 grid justify-items-center">
                         <img src="https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/3.png?v=1623465062" alt="easy return" />
                         <h3 className="uppercase font-semibold my-2">7 Days Easy Return</h3>
-                        <p className="text-base font-medium text-gray-600">Product any fault within 7 days for an immediate exchange</p>
+                        <p className="text-base font-medium text-gray-600 dark:text-white">Product any fault within 7 days for an immediate exchange</p>
                     </div>
                     <div className="col-span-1 grid justify-items-center">
                         <img src="https://fruityflavor-store-demo.myshopify.com/cdn/shop/files/4.png?v=1623465075" alt="quality" />
                         <h3 className="uppercase font-semibold my-2">Quality Guaranteed</h3>
-                        <p className="text-base font-medium text-gray-600">If your product aren't perfect, return them for a full refund</p>
+                        <p className="text-base font-medium text-gray-600 dark:text-white">If your product aren't perfect, return them for a full refund</p>
                     </div>
                 </div>
             </div>
             {/* /why choose section */}
             {/* best sellers section */}
             <div className="my-20">
-                <h2 className="text-5xl my-5 text-center">Our Best Sellers</h2>
-                <h4 className="font-medium text-gray-500 text-lg text-center">Best Seller Product This week</h4>
+                <h2 className="text-5xl my-5 text-center dark:text-white">Our Best Sellers</h2>
+                <h4 className="font-medium text-gray-500 text-lg text-center dark:text-white">Best Seller Product This week</h4>
                 <div className="mt-8 mx-10">
                     <div className="grid grid-cols-2 gap-8 md:grid-cols-2 lg:grid-cols-4">
                         {data.filterData === 0 && data.searchItem ? (
@@ -96,11 +96,11 @@ const Home = (data) => {
             </div>
             {/* /best sellers section */}
             {/* shop now section */}
-            <div className="bg-gray-100 grid grid-cols-2">
+            <div className="bg-gray-100 grid grid-cols-2 dark:text-white dark:bg-gray-950">
                 <div className="col-span-1 h-full flex flex-col items-center justify-center">
                     <h2 className="text-3xl text-center">Get Up To 15% Off <br /> <span className="text-5xl tracking-wider font-bold">Brain Food Studio</span></h2>
                     <h4 className="text-lg my-5 font-medium text-gray-500">Highest Quality</h4>
-                    <a href="/" target="_blank"><button type="button" className="bg-transparent border border-black py-4 px-10 font-medium hover:text-white hover:bg-black">Shop Now</button>
+                    <a href="/" target="_blank"><button type="button" className="bg-transparent dark:bg-white dark:text-black border border-black py-4 px-10 font-medium hover:text-white hover:bg-black dark:hover:bg-lime-500">Shop Now</button>
                     </a>
                 </div>
                 <div className="col-span-1">
--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -1,7 +1,81 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSun,
+  faMoon,
+} from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, faFacebook, faInstagram, faBehance } from "@fortawesome/free-brands-svg-icons";
+
 const Footer = () => {
+  function darkTheme() {
+    document.documentElement.classList.add("dark");
+  }
+  function lightTheme() {
+    document.documentElement.classList.remove("dark");
+  }
   return (
-    <footer className='bg-white dark:bg-slate-800 dark:text-white py-3 text-black text-center mt-auto'>
-      © All rights reserved<span className='text-amber-800 mx-2 dark:text-orange-500'>Real Food Store</span>{new Date().getFullYear()}.
+    <footer className="bg-gray-100 mt-10 dark:bg-slate-800 dark:text-white">
+      <div className="grid grid-cols-5 mx-10 py-10">
+        <div className="col-span-1">
+          <Link href="/" className="dark:hidden md:flex items-end">
+            <img
+              src="https://brainfoodstudio.com/wp-content/uploads/2018/07/brain-food-studio-logo-1200px.png"
+              alt="brain-food-logo"
+              className="md:w-36 w-24"
+            />
+          </Link>
+          <Link href="/" className="hidden dark:flex items-end">
+            <img
+              src="https://brainfoodstudio.com/wp-content/uploads/2018/07/brain-food-studio-logo-reverse-1200px.png"
+              alt="brain-food-logo"
+              className="md:w-36 w-24"
+            />
+          </Link>
+          <h3 className="mt-5 text-sm font-light">Sophisticated simplicity for <br />the independent mind.</h3>
+          <div>
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faBehance} />
+          </div>
+        </div>
+        <div className="col-span-1">
+          <h3 className="text-2xl font-medium">Quick Links</h3>
+          <ul className="mt-5 *:mb-3">
+            <li className="hover:font-bold"><a href="/home">Homepage</a></li>
+            <li className="hover:font-bold"><a href="/">Shop Now</a></li>
+            <li className="hover:font-bold"><a href="/cart">Cart</a></li>
+          </ul>
+        </div>
+        <div className="col-span-1">
+          <h3 className="text-2xl font-medium">Change Theme</h3>
+          <ul className="mt-5 *:mb-3">
+            <li><button
+              onClick={lightTheme}
+              type="button"
+              className="hover:font-bold">
+              <FontAwesomeIcon icon={faSun} />
+              <span className="ms-3">Light</span></button>
+            </li>
+            <li><button
+              onClick={darkTheme}
+              type="button"
+              className="hover:font-bold">
+              <FontAwesomeIcon icon={faMoon} />
+              <span className="ms-4">Dark</span></button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-span-2">
+          <h3 className="text-2xl font-medium">Our Newletters</h3>
+          <p className="my-5">Be the first who learns about our great promotions!</p>
+          <input type="text" className="py-2 px-4 w-72" placeholder="Enter your email..."/>
+          <button type="button" className="bg-black hover:bg-lime-500 text-white py-2 px-4">Subscribe</button>
+        </div>
+      </div>
+      <div className='bg-white dark:bg-slate-800 dark:text-white py-3 text-black text-center mt-auto'>
+        © All rights reserved<span className='text-amber-800 mx-2 dark:text-orange-500'>Real Food Store</span>{new Date().getFullYear()}.
+      </div>
     </footer>
   );
 };
