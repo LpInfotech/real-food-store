7dd1100a719db1a0a1983298a8da3a80aca7f4c2
gurleen124 <gurleen.kaur@lpinfotech.com>
2024-05-08 19:04:41 +0530

    optimized checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -162,34 +162,106 @@ const Checkout = () => {
 
     return (
         <>
-            <Navbar />
-            <div className="flex justify-between md:w-11/12 mx-auto items-center">
-                <h1 className="text-2xl font-bold my-4 text-center">My Shipping Address</h1>
-                <a href="/cart" target="_blank"><button type="button" className="hover:bg-lime-600 hover:text-white text-lime-600 border border-lime-600 font-bold py-2 px-4 rounded">Edit Cart</button></a>
-            </div>
-            <div className="bg-white shadow-md my-6 md:w-11/12 mx-auto rounded-md">
-                <form action="">
-                    <label htmlFor="country">Country</label>
-                    <select id="country" name="country">
-                        <option value="india">India</option>
-                        <option value="uk">United Kingdom</option>
-                        <option value="us">United States</option>
-                        <option value="canada">Canada</option>
-                        <option value="australia">Australia</option>
-                        <option value="nz">New Zealand</option>
-                        <option value="italy">Italy</option>
-                    </select>
-                </form>
-            </div>
-            <div className="flex justify-between mt-8 w-11/12 mx-auto">
-                <a href="/products" target="_blank" className="text-lime-500 hover:underline">
-                    Continue Shopping
-                </a>
-                <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">
-                    Checkout
-                </button>
-            </div>
-            <Footer />
+            <section className="bg-[url(https://oranda-store-demo.myshopify.com/cdn/shop/articles/blog2_1024x1024.jpg?v=1596786048)]">
+                <Navbar />
+                <div className="flex justify-end md:w-11/12 mx-auto items-center my-10">
+                    <a href="/cart" target="_blank"><button type="button" className="hover:bg-lime-600 hover:text-white text-lime-600 border border-lime-600 font-bold py-2 px-4 rounded">Edit Cart</button></a>
+                </div>
+                <div className="my-6 md:w-11/12 mx-auto rounded-lg grid grid-cols-3">
+                    <form action="" className="bg-white shadow-md py-10 items-center col-span-2 px-20">
+                        <h3 className="font-medium text-gray-500 py-3">Step 1/3</h3>
+                        <h1 className="uppercase text-2xl font-medium pb-5 text-lime-600">My Shipping Address</h1>
+                        <div className="gap-y-5 grid">
+                            <div className="grid grid-cols-2 gap-5">
+                                <h4 className="text-xl font-bold col-span-2 text-gray-500">Contact</h4>
+                                <div className="grid gap-3">
+                                    <label htmlFor="firstname" className="text-sm lg:text-xs xl:text-sm font-medium">First Name</label>
+                                    <input type="text" id="firstname" placeholder="Enter your first name" className="p-3 col-span-2 w-full bg-gray-100" />
+                                </div>
+                                <div className="grid gap-3">
+                                    <label htmlFor="lastname" className="text-sm lg:text-xs xl:text-sm font-medium">Last Name</label>
+                                    <input type="text" id="lastname" placeholder="Enter your last name" className="p-3 col-span-2 w-full bg-gray-100" />
+                                </div>
+                                <div className="grid gap-3">
+                                    <label htmlFor="phone" className="text-sm lg:text-xs xl:text-sm font-medium">Phone</label>
+                                    <input type="tel" id="phone" placeholder="Enter your phone number" className="p-3 col-span-2 w-full bg-gray-100" />
+                                </div>
+                                <div className="grid gap-3">
+                                    <label htmlFor="email" className="text-sm lg:text-xs xl:text-sm font-medium">Email Address</label>
+                                    <input type="email" id="email" placeholder="Enter your email address" className="p-3 col-span-2 w-full bg-gray-100" />
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-2 gap-5 mt-5">
+                                <h4 className="text-xl font-bold col-span-2 text-gray-500">Address</h4>
+                                <div className="grid gap-3">
+                                    <label htmlFor="country" className="text-sm lg:text-xs xl:text-sm font-medium">Country</label>
+                                    <select id="country" name="country" className="p-3 col-span-2 w-full bg-gray-100">
+                                        <option value="india">India</option>
+                                        <option value="uk">United Kingdom</option>
+                                        <option value="us">United States</option>
+                                        <option value="canada">Canada</option>
+                                        <option value="australia">Australia</option>
+                                        <option value="nz">New Zealand</option>
+                                        <option value="italy">Italy</option>
+                                    </select>
+                                </div>
+                                <div className="grid gap-3">
+                                    <label htmlFor="state" className="text-sm lg:text-xs xl:text-sm font-medium">State</label>
+                                    <input type="text" id="state" placeholder="Enter state" className="p-3 col-span-2 w-full bg-gray-100" />
+                                </div>
+                                <div className="grid gap-3 col-span-2">
+                                    <label htmlFor="address" className="text-sm lg:text-xs xl:text-sm font-medium">Full Address</label>
+                                    <input type="text" id="address" placeholder="Enter your full address" className="p-3 col-span-2 w-full bg-gray-100" />
+                                </div>
+                                <div>
+                                    <input type="checkbox" className="me-2" id="setAsDefault" name="setAsDefault" value="setAsDefault" />
+                                    <label htmlFor="setAsDefault"> Set as default</label>
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <button className="bg-lime-600 w-fit hover:bg-lime-700 text-white font-bold py-3 mt-10 px-5 rounded text-xl">
+                                    Save Address
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="mx-auto w-4/5">
+                        <div className="bg-white px-10 pb-10 rounded-lg">
+                            <h2 className="uppercase text-2xl font-medium pt-10 pb-5 text-lime-600">Order Summary</h2>
+                            <p className="pb-5">The total cost consists of the tax, Subtotal and the Shipping Charges</p>
+                            <hr className="border-gray-500 pt-5" />
+                            <div className="space-y-3">
+                                <p className="flex justify-between">
+                                    <span>Subtotal</span>
+                                    <span>$222</span>
+                                </p>
+                                <p className="flex justify-between">
+                                    <span>Taxes</span>
+                                    <span>$27</span>
+                                </p>
+                                <p className="flex justify-between">
+                                    <span>Shipping Charges</span>
+                                    <span>$28</span>
+                                </p>
+                                <hr className="border-gray-500" />
+                                <p className="flex justify-between text-2xl">
+                                    <span className="text-lime-600">Total</span>
+                                    <span>$277</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between mt-8 w-11/12 mx-auto">
+                    <a href="/products" target="_blank" className="text-lime-500 hover:underline">
+                        Continue Shopping
+                    </a>
+                    <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">
+                        Place Order
+                    </button>
+                </div>
+                <Footer />
+            </section>
         </>
     );
 };
